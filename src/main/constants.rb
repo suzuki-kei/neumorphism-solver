be9cbac070d5eb851134c1a13c6db20ac477a4c6
SUZUKI Kei <todokimasen@gmail.com>
2024-12-01 00:39:19 +0900

    コメントを追加した.

--- src/main/constants.rb
+++ src/main/constants.rb
@@ -1,8 +1,11 @@
 require 'pathname'
 
+# プロジェクトのルートディレクトリの絶対パス.
 ROOT_DIR = Pathname.new(File.absolute_path("#{File.dirname(__FILE__)}/../.."))
 
+# 問題ファイルを保持するディレクトリの絶対パス.
 DATA_DIR = ROOT_DIR.join('data')
 
+# キャッシュファイルを保持するディレクトリの絶対パス.
 CACHE_DIR = ROOT_DIR.join('cache')
 
